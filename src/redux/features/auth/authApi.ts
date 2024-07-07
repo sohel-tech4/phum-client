a147ab6cf863f8d7b38eb101970fffb617b966b4
Sohel Rana <soheltech4@gmail.com>
2024-07-08 02:16:54 +0600

    add authApi

---
+++ src/redux/features/auth/authApi.ts
@@ -0,0 +1,16 @@
+import { baseApi } from "../../api/baseApi";
+
+const authApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (userInfo) => ({
+        url: "/auth/login",
+        method: "POST",
+        body: userInfo,
+      }),
+    }),
+  }),
+  overrideExisting: false,
+});
+
+export const { useLoginMutation } = authApi;
